7009fa11380e3e368a4e570b26f9c6c22f1df7b4
colinah <halecolin1@gmail.com>
2020-03-18 16:51:56 -0600

    Save for later

--- sort/src/Canvas/Canvas.js
+++ sort/src/Canvas/Canvas.js
@@ -5,19 +5,22 @@ import Arr from './Support/makeArray'
 export default class Canvas extends Component {
   x = 50;
   y = 50;
-  cur = 0
+  cur = 0;
+  checker = 0;
+  xVal = 0;
+  running = true;
+  moveOn = false;
    
   sort = (array,cur) => {
       console.log('A',array, ' B', cur)
     for(let i= 0; i < 50; i++){
         if(cur !== i){
-            let initalX = array[i].x;
-            if(array[cur].x < array[i].x){
-                array[i].x = array[cur].x;
-                array[cur].x = initalX;
+            let inital = {...array[i]};
+            if(array[cur].h < array[i].h){
+                array[i] = {...array[cur]};
+                array[cur] = {...inital};
             }
         }
-        array[cur].color = 100; 
     }
     return array
 }
@@ -28,16 +31,54 @@ export default class Canvas extends Component {
 
   draw = (p5) => {
     p5.background(this.props.color);
+    p5.fill(255);
     p5.ellipse(this.x, this.y, 70, 70);
     // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
     this.x++;
+    if(this.running){
+    this.update(p5)
+    } else {
+      Arr.forEach((el)=>{
+        p5.fill(el.color);
+        p5.rect(this.xVal,el.y,el.w,el.h)
+        this.xVal += 10;
+        if(this.xVal > 500) this.xVal = 0; 
+    });
+    }
+
+  };
+
+  update = (p5) => {
+    Arr[this.cur].color = 100;
     Arr.forEach((el)=>{
         p5.fill(el.color);
-        p5.rect(el.x,el.y,el.w,el.h)
+        p5.rect(this.xVal,el.y,el.w,el.h)
+        this.xVal += 10;
     });
-    this.sort(Arr, this.cur);
-    if(this.cur < 49) {this.cur++ }
-  };
+    // Sort needs to be below here---
+    if(this.cur !== this.checker){
+      console.log('Arr:', Arr, ' cur: ', this.cur, " val:" , Arr[this.cur].h);
+      if(Arr[this.cur].h < Arr[this.checker].h){
+        console.log('hrh')
+        this.moveOn = true;
+        // let inital = {...Arr[this.checker]};
+        // Arr[this.checker] = {...Arr[this.cur]};
+        // Arr[this.cur] = {...inital};
+      }
+    }
+    //Sort above this line---
+    Arr[this.cur].color = 255;
+    if(this.cur < 50) {
+      if(this.moveOn){
+        this.cur++;
+        this.checker = this.cur;
+      }
+      this.xVal = 0;
+      this.checker ++
+    } else {
+      this.running = false
+    }
+  }
  
   render() {
     return <Sketch setup={this.setup} draw={this.draw}/>;
--- sort/src/Canvas/Support/makeArray.js
+++ sort/src/Canvas/Support/makeArray.js
@@ -2,14 +2,14 @@
     xVal = 0,
     yVal = 500,
     width = 10;
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 51; i++){
     arr.push({x:xVal,y:yVal,w:width,h:500-yVal,color:255})
     xVal += 10;
     yVal -= 10;
     }
     
-    const shuffle = (array) => {
-        for(let i= 0; i < 50; i++){
+    const shuffleX = (array) => {
+        for(let i= 0; i < array.length; i++){
             let initalX = array[i].x;
             let r = Math.floor(Math.random()*50)
             array[i]['x'] = array[r]['x'];
@@ -18,6 +18,18 @@
         return array
     }
 
-    let shuf = shuffle(arr)
+    function shuffleArray (array) {
+        var j, x, i;
+        for (i = array.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = array[i];
+            array[i] = array[j];
+            array[j] = x;
+        }
+        return array;
+    }
+
+    // let shuf = shuffleX(arr)
+    let shuf = shuffleArray(arr)
 
     export default shuf
